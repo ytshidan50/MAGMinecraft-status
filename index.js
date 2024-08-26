6597c71beb76f258c6e57f6f9cbc1ff28e79d9a2
SHIDAN V <111380045+ytshidan50@users.noreply.github.com>
2024-08-27 02:01:31 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Playing play.magmc.xyz"];
 
 
 let currentIndex = 0;
